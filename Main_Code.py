2da761b2b9dc59c6b0555e727b7a852044ec1d99
dushyantRathore <dushyant.bgs@gmail.com>
2016-12-16 19:31:28 +0530

    Updated Code

--- Main_Code.py
+++ Main_Code.py
@@ -4,6 +4,7 @@ import urllib2
 import pandas as pd
 import numpy as np
 
+
 # Entry Function
 def Entry():
 
@@ -159,7 +160,7 @@ def bundesliga():
     # League Standings
     if a == '1':
         url = "http://www.espn.in/football/table/_/league/ger.1"
-        # leagueStandings_bundesliga(url)
+        leagueStandings_bundesliga(url)
 
     # Top Scorers
     elif a == '2':
@@ -262,69 +263,69 @@ def leagueStandings(url):
 
 
 # Function to fetch the League Standings for Bundesliga
-# def leagueStandings_bundesliga(url):
-#     url = str(url)
-#     contest_file = urllib2.urlopen(url)
-#     contest_html = contest_file.read()
-#     contest_file.close()
-#
-#     soup = BeautifulSoup(contest_html, 'html.parser')
-#
-#     row = soup.find_all("tr", attrs={'class': 'standings-row'})
-#
-#     table = []
-#
-#     for i in row:
-#         team = []
-#         for j in i:
-#             team.append(j.text)
-#             if len(team) == 9:
-#                 table.append(team)
-#
-#     team_name = []
-#     games_played = []
-#     wins = []
-#     draws = []
-#     losses = []
-#     goals_for = []
-#     goals_against = []
-#     goals_difference = []
-#     points = []
-#
-#     for i in range(0, 18):
-#         x = str(i + 1)
-#         team_name.append(table[i][0].strip(x))
-#         games_played.append(table[i][1])
-#         wins.append(table[i][2])
-#         draws.append(table[i][3])
-#         losses.append(table[i][4])
-#         goals_for.append(table[i][5])
-#         goals_against.append(table[i][6])
-#         goals_difference.append(table[i][7])
-#         points.append(table[i][8])
-#
-#     sequence = ["Position", "Team", "Games", "Wins", "Draws", "Losses", "For", "Against", "Goals Difference",
-#                 "Points"]
-#     df = pd.DataFrame()
-#     df = df.reindex(columns=sequence)
-#
-#     pos = []
-#     for i in range(1, 21):
-#         pos.append(i)
-#
-#     df["Position"] = pos
-#     df["Team"] = team_name
-#     df["Games"] = games_played
-#     df["Wins"] = wins
-#     df["Draws"] = draws
-#     df["Losses"] = losses
-#     df["For"] = goals_for
-#     df["Against"] = goals_against
-#     df["Goals Difference"] = goals_difference
-#     df["Points"] = points
-#
-#     print "\n"
-#     print df.to_string()
+def leagueStandings_bundesliga(url):
+    url = str(url)
+    contest_file = urllib2.urlopen(url)
+    contest_html = contest_file.read()
+    contest_file.close()
+
+    soup = BeautifulSoup(contest_html, 'html.parser')
+
+    row = soup.find_all("tr", attrs={'class': 'standings-row'})
+
+    table = []
+
+    for i in row:
+        team = []
+        for j in i:
+            team.append(j.text)
+            if len(team) == 9:
+                table.append(team)
+
+    team_name = []
+    games_played = []
+    wins = []
+    draws = []
+    losses = []
+    goals_for = []
+    goals_against = []
+    goals_difference = []
+    points = []
+
+    for i in range(0, 18):
+        x = str(i + 1)
+        team_name.append(table[i][0].strip(x))
+        games_played.append(table[i][1])
+        wins.append(table[i][2])
+        draws.append(table[i][3])
+        losses.append(table[i][4])
+        goals_for.append(table[i][5])
+        goals_against.append(table[i][6])
+        goals_difference.append(table[i][7])
+        points.append(table[i][8])
+
+    sequence = ["Position", "Team", "Games", "Wins", "Draws", "Losses", "For", "Against", "Goals Difference",
+                "Points"]
+    df = pd.DataFrame()
+    df = df.reindex(columns=sequence)
+
+    pos = []
+    for i in range(1, 19):
+        pos.append(i)
+
+    df["Position"] = pos
+    df["Team"] = team_name
+    df["Games"] = games_played
+    df["Wins"] = wins
+    df["Draws"] = draws
+    df["Losses"] = losses
+    df["For"] = goals_for
+    df["Against"] = goals_against
+    df["Goals Difference"] = goals_difference
+    df["Points"] = points
+
+    print "\n"
+    print df.to_string()
 
 
 # Function to fetch the Top Scorers
