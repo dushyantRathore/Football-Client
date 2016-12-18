3c010e1dbddc82e6af13b344b7dde4d2ed72bc33
dushyantRathore <dushyant.bgs@gmail.com>
2016-12-18 12:17:55 +0530

    Updated Code

--- Main_Code.py
+++ Main_Code.py
@@ -100,39 +100,44 @@ def epl():
 
 # Laliga function
 def laliga():
+    print '\033[96m' + "\nWelcome to English Premier League" + '\033[00m'
 
-    print '\033[96m' + "\nWelcome to Spanish Primera Division" + '\033[00m'
-
-    print '\033[94m' + "\nPress 1 for League Standings"
-    print "Press 2 for Top Scorers"
-    print "Press 3 for Top Assists"
-    print "Press 4 for Discipline"
-    print "Press 5 for Fairplay" + '\033[00m'
+    print '\033[94m' + "\nPress 1 for League Fixtures"
+    print "Press 2 for League Standings"
+    print "Press 3 for Top Scorers"
+    print "Press 4 for Top Assists"
+    print "Press 5 for Discipline"
+    print "Press 6 for Fairplay" + '\033[00m'
 
     a = raw_input('\033[1m' + "\nEnter your choice : " + '\033[00m')
 
-    # League Standings
+    # League Fixtures
     if a == '1':
+        url = "http://www.bbc.com/sport/football/spanish-la-liga/fixtures"
+        getFixtures(url)
+
+    # League Standings
+    elif a == '2':
         url = "http://www.espn.in/football/table/_/league/esp.1"
         leagueStandings(url)
 
     # Top Scorers
-    elif a == '2':
+    elif a == '3':
         url = "http://www.espnfc.com/spanish-primera-division/15/statistics/scorers"
         topScorers(url)
 
     # Top Assists
-    elif a == '3':
+    elif a == '4':
         url = "http://www.espnfc.com/spanish-primera-division/15/statistics/assists"
         topAssists(url)
 
     # Discipline
-    elif a == '4':
+    elif a == '5':
         url = "http://www.espnfc.com/spanish-primera-division/15/statistics/discipline"
         discipline(url)
 
     # Fairplay
-    elif a == '5':
+    elif a == '6':
         url = "http://www.espnfc.com/spanish-primera-division/15/statistics/fairplay"
         fairplay(url)
 
